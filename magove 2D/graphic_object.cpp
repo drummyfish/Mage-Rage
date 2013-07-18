5050d8542cfc177f9441d6b62557ca9f3e1b6749
Miloslav Číž <tastyfish@seznam.cz>
2013-07-18 10:59:14 +0200

    pridan c_graphic_object

---
+++ magove 2D/graphic_object.cpp
@@ -0,0 +1,41 @@
+﻿/**
+ * Graphic object class implementation.
+ *
+ * authors: Miloslav Číž, Martin Gabriel
+ * year: 2013
+ */
+
+#include "graphic_object.h"
+
+//--------------------------------------------------
+
+void c_graphic_object::draw(int x, int y)
+  {
+  }
+
+//--------------------------------------------------
+
+void c_graphic_object::play_animation(int animation_id)
+  {
+	this->playing_animation = animation_id;
+	this->animation_frame = 0;
+	this->looping_animation = false;
+  }
+
+//--------------------------------------------------
+
+void c_graphic_object::loop_animation(int animation_id)
+  {
+	this->playing_animation = animation_id;
+	this->animation_frame = 0;
+	this->looping_animation = true;
+  }
+
+//--------------------------------------------------
+
+void c_graphic_object::stop_animation()
+  {
+    this->playing_animation = -1;
+  }
+
+//--------------------------------------------------
---
+++ magove 2D/graphic_object.h
@@ -0,0 +1,64 @@
+﻿#ifndef GRAPHIC_OBJECT
+#define GRAPHIC_OBJECT
+
+/**
+ * Graphic object class header file.
+ *
+ * authors: Miloslav Číž, Martin Gabriel
+ * year: 2013
+ */
+
+using namespace std;
+
+class c_graphic_object
+  {
+	/**
+	  This class represents an object that is able to
+	  draw itself on the screen. It can also play
+	  animations.
+	*/
+
+    private:
+	  int playing_animation;  /// Id of the current animation, -1 means no animation.
+	  int animation_frame;    /// Current animation frame. 
+	  bool looping_animation; /// True if the animation is looping, false otherwise.
+
+    public:
+
+      virtual void c_graphic_object::draw(int x, int y);
+
+	    /**
+	      Tells the object to draw itself at given
+	  	  coordinations on the screen.
+
+		  @param x x coordination of the screen
+		  @param y y coordination of the screen
+	    */
+
+	  void play_animation(int animation_id);
+
+	    /**
+		  Plays given animation.
+
+		  @param animation_id id of the animation,
+		    which must not be negative
+		*/
+
+	  void loop_animation(int animation_id);
+
+	    /**
+		  Loops the given animation untill it's
+		  stopped by stop_animation().
+
+		  @param animation_id id of the animation,
+		    which must not be negative
+		*/
+
+	  void stop_animation();
+	    
+	    /**
+		  Stops playing the current animation.
+		*/
+  };
+
+#endif
